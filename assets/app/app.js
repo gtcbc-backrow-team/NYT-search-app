122ddd8b156580b108e2f37f2b59123773718b21
Chance Phiniezy <phiniezyc@gmail.com>
2017-09-18 20:32:07 -0400

    added search Term button functionality, others need to be added

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,1 +1,25 @@
-console.log("Test");
+//var searchTerm = document.getElementById('search-term');
+
+
+$("#searchButton").click(function() {
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "477aaaa9fcad46968341169b0db2ef96",
+      "q": document.getElementById('search-term').value,
+      "begin_date": "19990410",
+      "end_date": "20160410"
+    });
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+      console.log(result);
+    }).fail(function(err) {
+      throw err;
+    });
+    
+    //url.q = "Atlanta";
+    //console.log(document.getElementById("search-term").value
+
+})
+
